fba2df8164dea7dbbafe6b5dea56ffa3b9188e37
Danil <11051969dahul@mail.ru>
2021-10-02 01:08:34 +0500

    made the display of employees, but there is a problem with the display time, I think to make an API for the frontend, or add forms

--- staff_tree/apps/staff/views.py
+++ staff_tree/apps/staff/views.py
@@ -2,13 +2,26 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from staff_tree.apps.staff.generator import Generator
-from staff.models import Employee
+from subdivisions.models import Subdivision, Level
 
 
 def tree(request):
     count_users_in_db = User.objects.count()
     Generator.create_users(count_users_in_db=count_users_in_db)
 
-    all_employees = Employee.objects.all()
+    all_levels = Level.objects.all()
 
-    return render(request, 'staff/tree.html', {'all_employees': all_employees})
+    dict_tree_employees = {}
+    all_subdivisions = Subdivision.objects.all()
+    for subdivision in all_subdivisions:
+        employees_in_subdivision = subdivision.employee_set.all()
+
+        dict_levels_and_employees = {}
+        for level in all_levels:
+            dict_levels_and_employees.update(
+                {level: employees_in_subdivision.filter(level_in_subdivision=level).all()}
+            )
+
+        dict_tree_employees.update({subdivision: dict_levels_and_employees})
+
+    return render(request, 'staff/tree.html', {'dict_tree_employees': dict_tree_employees})
