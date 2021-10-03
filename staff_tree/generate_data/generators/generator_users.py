0b8d55447e0db7217270c1fa2673fb914e53156f
Danil <11051969dahul@mail.ru>
2021-10-03 15:21:50 +0500

    add function random string in generator users

--- staff_tree/generate_data/generators/generator_users.py
+++ staff_tree/generate_data/generators/generator_users.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 
 from staff_tree.generate_data.generators import BaseGenerator
 
+import random
+import string
+
 
 class GeneratorUsers(BaseGenerator):
     MAX_COUNT_IN_DATABASE = 50000
@@ -10,6 +13,12 @@ class GeneratorUsers(BaseGenerator):
         self.count_models_in_db = count_models_in_db
 
     def generate(self):
-        users = [User(username=i, password=f"{i}super-super")
+        users = [User(username=self._generate_random_string(10) + "_" + str(i), password=f"{i}super-super")
                  for i in range(self.count_models_in_db, self.MAX_COUNT_IN_DATABASE)]
         return users
+
+    @staticmethod
+    def _generate_random_string(length) -> str:
+        letters = string.ascii_lowercase
+        rand_string = ''.join(random.choice(letters) for i in range(length))
+        return rand_string
